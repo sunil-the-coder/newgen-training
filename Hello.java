108e4d5d7f5198f0a552d6c8ce353401b7c3bf44
Sunil Patil <sunilpatil10june@gmail.com>
2025-01-16 06:46:01 +0530

    Day-1 Hello World application

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello {
+    public static void main(String[] args) {
+        
+        //Identifier -> variable, class, method etc.
+
+        double number1 = 10.53523;
+        double number2 = 20.55252;
+        
+        double result = number1 + number2;
+
+        //10.5f, 'c', 10.564364436
+
+        System.out.println(result);
+        
+        // "Hello Java" -> String
+        // 'c' => character
+
+       
+    }
+}
+
